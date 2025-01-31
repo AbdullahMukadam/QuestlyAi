97b00985e8f7333f12aad4efccf8965502996e72
AbdullahMukadam <abdullahmukadam21@gmail.com>
2025-01-31 07:02:40 -0800

    added the video element

--- src/app/components/landing-page/LandingPage.js
+++ src/app/components/landing-page/LandingPage.js
@@ -5,12 +5,14 @@ import { GoArrowUpRight } from 'react-icons/go';
 import Script from 'next/script';
 import SkeletonLoader from './loader';
 import Image from 'next/image';
+import HeroVideoDialog from '@/components/ui/hero-video-dialog';
+import { BookmarkCheck, Database, LockKeyhole } from 'lucide-react';
 
 function LandingPage() {
   const [loading, setloading] = useState(true)
 
   const ImageStyle = {
-    width : "100%"
+    width: "100%"
   }
 
   useEffect(() => {
@@ -36,9 +38,9 @@ function LandingPage() {
       {/* Responsive container for the model-viewer */}
       <div className="model-viewer-container hidden md:block">
         <model-viewer
-          src="/models/Baymax.glb" 
+          src="/models/Baymax.glb"
           alt="A 3D model"
-          camera-controls    
+          camera-controls
           auto-rotate
           style={{ width: '100%', height: '100%' }}
         ></model-viewer>
@@ -64,16 +66,17 @@ function LandingPage() {
               Learn More <span>{<GoArrowUpRight />}</span>
             </Button>
           </section>
+
         </div>
       </section>
       <section className='w-full mt-4 relative flex justify-center md:hidden'>
         <Image
-        src={"/demo_image.jfif"}
-        alt='DashBoard'
-        width={400}
-        height={300}
-        objectFit='contain'
-        loading='lazy'
+          src={"/demo_image.jfif"}
+          alt='DashBoard'
+          width={400}
+          height={300}
+          objectFit='contain'
+          loading='lazy'
         />
       </section>
       <section className="w-full mt-8">
@@ -85,6 +88,45 @@ function LandingPage() {
               Understand that Trditional methods can be time consuming and hinder the ability to
               truly Identify the best talent.
             </p>
+          </div>
+          <div className='w-full relative p-1'>
+            <HeroVideoDialog
+              className="dark:hidden block h-full"
+              animationStyle="from-center"
+              videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+              thumbnailSrc="https://startup-template-sage.vercel.app/hero-light.png"
+              thumbnailAlt="Hero Video"
+            />
+            <HeroVideoDialog
+              className="hidden h-full dark:block"
+              animationStyle="from-center"
+              videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+              thumbnailSrc="https://startup-template-sage.vercel.app/hero-dark.png"
+              thumbnailAlt="Hero Video"
+            />
+          </div>
+          <div className='w-full mt-2 p-1 flex items-center justify-center gap-4 flex-col md:flex-row'>
+            <div className='w-full p-3 rounded-2xl bg-slate-50'>
+              <LockKeyhole size={37} className=' bg-[#18B088] text-white rounded-sm p-2' />
+              <div className='w-full p-1 text-start mt-3'>
+                <h3 className='text-xl mb-2 font-bold'>UnBiased Insights</h3>
+                <p className='text-gray-500 text-sm font-bold'>Advanced Algorithms Assess skills providing you with an accurate representation of each candidate.</p>
+              </div>
+            </div>
+            <div className='w-full p-3 rounded-2xl bg-slate-50'>
+              <BookmarkCheck size={37} className=' bg-[#18B088] text-white rounded-sm p-2' />
+              <div className='w-full p-1 text-start mt-3'>
+                <h3 className='text-xl mb-2 font-bold'>Efficient Evaluation</h3>
+                <p className='text-gray-500 text-sm font-bold'>Save Time By identifying top candidates quickly, automated analysis of technical skills, problem solving abilities and more.</p>
+              </div>
+            </div>
+            <div className='w-full p-3 rounded-2xl bg-slate-50'>
+              <Database size={37} className=' bg-[#18B088] text-white rounded-sm p-2' />
+              <div className='w-full p-1 text-start mt-3'>
+                <h3 className='text-xl mb-2 font-bold'>Data-Driven Decisions</h3>
+                <p className='text-gray-500 text-sm font-bold'>Comprehensive view of candidate performance across interview enabling you to choose best candidates.</p>
+              </div>
+            </div>
           </div>
         </div>
       </section>
